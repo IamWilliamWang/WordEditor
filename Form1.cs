7f5595909e2dcdba784066e2aa3b3d944cf6dc05
IamWilliamWang <iamjerichoholic@hotmail.com>
2016-10-25 09:46:21 +0800

    完成了所有word的基础功能

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+using DevExpress.XtraBars.Ribbon;
+using DevExpress.XtraBars.Helpers;
+using DevExpress.Skins;
+using DevExpress.LookAndFeel;
+using DevExpress.UserSkins;
+
+
+namespace WordEditor
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitSkinGallery();
+            InitializeRichEditControl();
+            ribbonControl.SelectedPage = homeRibbonPage1;
+        }
+        void InitSkinGallery()
+        {
+            SkinHelper.InitSkinGallery(rgbiSkins, true);
+        }
+        void InitializeRichEditControl()
+        {
+
+        }
+        private void IExit_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Application.Exit();
+        }
+        private void IAbout_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            MessageBox.Show("Word文档编辑器\n支持docx doc rtf txt htm html mht odt xml epub文件","关于");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using DevExpress.LookAndFeel;
+using DevExpress.Skins;
+using System.Drawing;
+
+namespace WordEditor
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            DevExpress.Skins.SkinManager.EnableFormSkins();
+            DevExpress.UserSkins.BonusSkins.Register();
+            UserLookAndFeel.Default.SetSkinStyle("Office 2010 Blue");
+
+            Application.Run(new Form1());
+        }
+    }
+}
